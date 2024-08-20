22184dce7fd26f99ed611fc8f6c6fda2118dac67
최하늘 <chn0625@mhqglobal.com>
2024-08-20 10:33:23 +0900

    first commit

---
+++ Haruhanjul/HaruhanjulApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaruhanjulApp.swift
+//  Haruhanjul
+//
+//  Created by 최하늘 on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HaruhanjulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
