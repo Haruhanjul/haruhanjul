20d3740885d1e29cb9d7835cb45e3c679c74788a
최하늘 <chn0625@mhqglobal.com>
2024-08-26 14:18:12 +0900

    style: 주석 추가

--- HaruhanjulWidget/HaruhanjulWidget.swift
+++ HaruhanjulWidget/HaruhanjulWidget.swift
@@ -9,35 +9,52 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: AppIntentTimelineProvider {
+    
+    // 위젯이 초기 상태에서 데이터를 로드하기 전, 임시로 표시할 내용을 제공
+    // 위젯을 추가할 때 로딩 중에 보여질 기본적인 뷰 생성
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
     }
 
+    // 위젯 갤러리에서 위젯을 미리볼 때 사용
+    // 실제 데이터를 가져오기 전에 빠르게 표시할 수 있는 샘플 데이터를 제공하거나 API를 통해 데이터를 가져오는 데 시간이 걸릴 경우 하드코딩된 데이터를 사용
+    // context.isPreview가 true인 경우 위젯 갤러리에서 미리보기를 제공할 때 사용
     func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: configuration)
     }
     
+    // 위젯이 홈화면에서 언제 어떤 데이터를 표시할 지 결정
+    // 위젯의 타임라인을 구성하고 타임라인 내의 각 항목(엔트리)이 어떤 시점에 표시될 지 정의
     func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
         var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        // 현재 날짜부터 시작하여 1시간 간격으로 5개의 항목으로 구성된 타임라인을 생성합니다.
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
+            // 1시간뒤, 2시간뒤, ... 4시간뒤 엔트리 값으로 업데이트
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
             let entry = SimpleEntry(date: entryDate, configuration: configuration)
             entries.append(entry)
         }
 
+        // `policy: .atEnd는 타임라인의 마지막 항목이 표시된 후, 위젯이 더 이상 갱신되지 않음을 의미
         return Timeline(entries: entries, policy: .atEnd)
     }
 }
 
 struct SimpleEntry: TimelineEntry {
+    // 위젯이 언제 해당 항목을 화면에 표시할 지를 정함
+    // 각 타임라인 항목이 특정 시점에 표시될 시간을 정의하고 시점에 맞춰 내용을 업데이트
     let date: Date
+    
+    // 위젯에 대한 사용자 설정, 사용자가 위젯 설정 화면에서 지정한 설정 값을 포함
+    // 이 설정 값은 위젯의 UI나 동작에 영향을 미침
     let configuration: ConfigurationAppIntent
 }
 
 struct HaruhanjulWidgetEntryView : View {
+    // 위젯의 데이터 모델로, 각 항목의 표시될 정보가 포함되어 있음
     var entry: Provider.Entry
 
     var body: some View {
@@ -52,13 +69,24 @@ struct HaruhanjulWidgetEntryView : View {
 }
 
 struct HaruhanjulWidget: Widget {
+    // 위젯의 고유 식별자
     let kind: String = "HaruhanjulWidget"
 
     var body: some WidgetConfiguration {
-        AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
+        // IntentConfiguration: 사용자가 위젯에서 Edit을 통해 위젯에 보여지는 내용 변경이 가능
+        // StaticConfiguration: 사용자가 변경 불가능한 정적 데이터 표출
+        AppIntentConfiguration(
+            kind: kind, // 위젯 ID
+            intent: ConfigurationAppIntent.self, // 사용자가 위젯을 설정하는 인텐트
+            provider: Provider() // 위젯 생성자: 언제 어떤 데이터를 표시할지 결정
+        ) { entry in
+            
+            // 실제 표시될 UI 정의
             HaruhanjulWidgetEntryView(entry: entry)
                 .containerBackground(.fill.tertiary, for: .widget)
         }
+        .configurationDisplayName("하루 한줄") // 위젯 설정 화면에서 표시될 위젯의 이름
+        .description("하루에 한줄씩 읽어보세요.") // 위젯에 대한 설명을 정의
     }
 }
 
--- HaruhanjulWidget/HaruhanjulWidgetLiveActivity.swift
+++ HaruhanjulWidget/HaruhanjulWidgetLiveActivity.swift
@@ -9,12 +9,18 @@ import ActivityKit
 import WidgetKit
 import SwiftUI
 
+
+// 활동의 속성과 상태를 정의하는 데 사용
+// 동적이고 지속적인 콘텐츠를 제공하는 기능
 struct HaruhanjulWidgetAttributes: ActivityAttributes {
+    
+    // 활동의 동적 상태를 정의
     public struct ContentState: Codable, Hashable {
         // Dynamic stateful properties about your activity go here!
         var emoji: String
     }
 
+    // 활동의 정적 상태를 정의
     // Fixed non-changing properties about your activity go here!
     var name: String
 }
@@ -22,6 +28,8 @@ struct HaruhanjulWidgetAttributes: ActivityAttributes {
 struct HaruhanjulWidgetLiveActivity: Widget {
     var body: some WidgetConfiguration {
         ActivityConfiguration(for: HaruhanjulWidgetAttributes.self) { context in
+            
+            // 위젯의 잠금화면 또는 배너에서 표시될 UI를 정의
             // Lock screen/banner UI goes here
             VStack {
                 Text("Hello \(context.state.emoji)")
@@ -30,6 +38,8 @@ struct HaruhanjulWidgetLiveActivity: Widget {
             .activitySystemActionForegroundColor(Color.black)
 
         } dynamicIsland: { context in
+            
+            // 다이나믹 아일랜드의 UI를 정의
             DynamicIsland {
                 // Expanded UI goes here.  Compose the expanded UI through
                 // various regions, like leading/trailing/center/bottom
