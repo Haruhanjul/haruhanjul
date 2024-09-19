d7c4cc25e0aae76c5597f9a5efbacf63a67a946b
최하늘 <chn0625@mhqglobal.com>
2024-09-19 10:45:46 +0900

    fix: 메인 스레드 오류 수정

--- Haruhanjul/Presentation/Card/ViewModel/CardViewModel.swift
+++ Haruhanjul/Presentation/Card/ViewModel/CardViewModel.swift
@@ -12,65 +12,71 @@ final class CardViewModel: ObservableObject {
     @Published var advices: [AdviceEntity] = []
     @Published var isLoading: Bool = false
     
-    let englishKoreanTranslator = Translator.translator(options: TranslatorOptions(sourceLanguage: .english, targetLanguage: .korean))
-    
-    let conditions = ModelDownloadConditions(
-        allowsCellularAccess: false, // 셀룰러 허용 여부
-        allowsBackgroundDownloading: true // 백그라운드 다운 허용 여부
+    private let englishKoreanTranslator = Translator.translator(options: TranslatorOptions(sourceLanguage: .english, targetLanguage: .korean))
+    private let conditions = ModelDownloadConditions(
+        allowsCellularAccess: false,
+        allowsBackgroundDownloading: true
     )
     
     init() {
         self.englishKoreanTranslator.downloadModelIfNeeded(with: conditions) { error in
-            guard error == nil else { return }
+            if let error = error {
+                print("Error downloading model: \(error)")
+            }
         }
     }
     
-    func loadAdvices(count: Int) async {
+    func loadAdvices(count: Int) {
         let urlString = "https://api.adviceslip.com/advice"
-        let session = URLSession.shared
-        let dispatchGroup = DispatchGroup()
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL")
+            return
+        }
         
         self.isLoading = true
         
+        let dispatchGroup = DispatchGroup()
+        
         for _ in 0..<count {
-            guard let url = URL(string: urlString) else {
-                print("Invalid URL")
-                return
-            }
-            
             let request = URLRequest(url: url)
             
             dispatchGroup.enter()
             
-            let task = session.dataTask(with: request) { [self] (data, response, error) in
+            let task = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
                 defer { dispatchGroup.leave() }
                 
-                guard error == nil else {
-                    print("Error: \(error!)")
-                    return
-                }
-                guard let httpResponse = response as? HTTPURLResponse else {
-                    print("Invalid httpResponse")
+                guard let self = self else { return }
+                
+                if let error = error {
+                    print("Error: \(error)")
                     return
                 }
                 
-                if (200..<300).contains(httpResponse.statusCode) {
-                    if let data = data {
-                        do {
-                            let response = try JSONDecoder().decode(AdviceDTO.self, from: data)
-                            let advice = response.slip.convertToEntity()
-                            self.translateToKorean(with: advice)
-                            self.advices.append(advice)
-                            
-                        } catch {
-                            print("Error decoding JSON: \(error)")
+                guard let httpResponse = response as? HTTPURLResponse, (200..<300).contains(httpResponse.statusCode) else {
+                    print("Invalid response or status code")
+                    return
+                }
+                
+                guard let data = data else {
+                    print("No data")
+                    return
+                }
+                
+                do {
+                    let response = try JSONDecoder().decode(AdviceDTO.self, from: data)
+                    let advice = response.slip.convertToEntity()
+                    
+                    translateToKorean(advice) { translatedText in
+                        var updatedAdvice = advice
+                        updatedAdvice.adviceKorean = translatedText
+                        
+                        DispatchQueue.main.async { [weak self] in
+                            guard let self else { return }
+                            advices.append(updatedAdvice)
                         }
                     }
-                } else {
-                    if let data = data {
-                        let responseData = String(data: data, encoding: .utf8)
-                        print("\(httpResponse.statusCode) data: \(responseData ?? "No data")")
-                    }
+                } catch {
+                    print("Error decoding JSON: \(error)")
                 }
             }
             task.resume()
@@ -81,12 +87,14 @@ final class CardViewModel: ObservableObject {
         }
     }
     
-    private func translateToKorean(with advice: AdviceEntity) {
-        guard let index = advices.firstIndex(where: { $0.id == advice.id }) else { return }
-        englishKoreanTranslator.translate(advice.content) { [weak self] translatedText, error in
-            guard let self else { return }
-            guard error == nil, let translatedText else { return }
-            advices[index].adviceKorean = translatedText
+    private func translateToKorean(_ advice: AdviceEntity, completion: @escaping (String) -> Void) {
+        self.englishKoreanTranslator.translate(advice.content) { translatedText, error in
+            if let error = error {
+                print("Translation error: \(error)")
+                completion("")
+            } else {
+                completion(translatedText ?? "")
+            }
         }
     }
 }
