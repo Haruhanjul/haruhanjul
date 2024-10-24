68a2fad45d2cfcba1d53b3354d979ce23c6d4be2
DAEJINLIM <daejin.limm@gmail.com>
2024-10-25 03:53:27 +0900

    feat: 백그라운드 작업 자동 예약 설정

--- HaruhanjulApp/Haruhanjul/App/HaruhanjulApp.swift
+++ HaruhanjulApp/Haruhanjul/App/HaruhanjulApp.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 import BackgroundTasks
 
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: "com.daehaa.Haruhanjul.refresh", using: nil) { task in
+            self.handleAdviceRefresh(task: task as! BGAppRefreshTask)
+        }
+        return true
+    }
+    
+    // 백그라운드 작업 예약
+    func scheduleAdviceRefreshTask() {
+        let request = BGAppRefreshTaskRequest(identifier: "com.daehaa.Haruhanjul.refresh")
+
+        let currentDate = Date()
+        let calendar = Calendar.current
+
+        let today = calendar.startOfDay(for: currentDate)
+
+        let nextDay = calendar.date(byAdding: .day, value: 1, to: today)!
+        let scheduleTime = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: today)!
+
+        request.earliestBeginDate = scheduleTime
+
+        do {
+            try BGTaskScheduler.shared.submit(request)
+//            print("[BGTaskScheduler] submitted task with id: \(request.identifier)")
+        } catch let error {
+//            print("[BGTaskScheduler] error:", error)
+        }
+    }
+
+    // 백그라운드 작업이 트리거될 때 호출되는 함수
+    func handleAdviceRefresh(task: BGAppRefreshTask) {
+        CardViewModel.shared.removeAdvice(at: 0)
+        
+        task.setTaskCompleted(success: true)
+        
+        scheduleAdviceRefreshTask()
+    }
+}
+
 @main
 struct HaruhanjulApp: App {
     @State var isSplashView = true
-    @StateObject var cardViewModel = CardViewModel()
     @Environment(\.scenePhase) private var scenePhase
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     
     let adviceRefreshIdentifier = "com.daehaa.Haruhanjul.refresh"
     let persistenceController = PersistenceController.shared
@@ -30,38 +71,12 @@ struct HaruhanjulApp: App {
             } else {
                 MainTabView()
                     .environment(\.managedObjectContext, persistenceController.container.viewContext)
-                    .environmentObject(cardViewModel)
             }
         }
         .onChange(of: scenePhase) { newValue in
             if newValue == .background {
-                scheduleAdviceRefreshTask()
+                appDelegate.scheduleAdviceRefreshTask()
             }
-        }
-        .backgroundTask(.appRefresh(adviceRefreshIdentifier)) {
-            await cardViewModel.removeAdvice(at: 0)
-        }
-    }
-    
-    // 매일 명언 업데이트 백그라운드 작업
-    private func scheduleAdviceRefreshTask() {
-        let request = BGAppRefreshTaskRequest(identifier: adviceRefreshIdentifier)
-
-        let currentDate = Date()
-        let calendar = Calendar.current
-
-        let today = calendar.startOfDay(for: currentDate)
-
-        let nextDay = calendar.date(byAdding: .day, value: 1, to: today)!
-        let scheduleTime = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: nextDay)!
-
-        request.earliestBeginDate = scheduleTime
-
-        do {
-            try BGTaskScheduler.shared.submit(request)
-            print("[BGTaskScheduler] submitted task with id: \(request.identifier)")
-        } catch let error {
-            print("[BGTaskScheduler] error:", error)
         }
     }
 }
