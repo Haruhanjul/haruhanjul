caea2b68334c09bd85e6eaf582615cf3f306f290
HANLeeeee <hanleeeee@naver.com>
2024-10-28 00:12:27 +0900

    fix: alamofire 추가 오류 수정

---
+++ NetworkKit/NetworkKit/Endpoint/Requestable.swift
@@ -0,0 +1,25 @@
+//
+//  Requestable.swift
+//  NetworkKit
+//
+//  Created by 최하늘 on 10/25/24.
+//
+
+import Foundation
+
+internal import Alamofire
+
+typealias HTTPRequestParameter = [String: Any]
+
+protocol Requestable {
+    
+    associatedtype Response: Decodable
+    
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: HTTPHeaders { get }
+    var parameters: HTTPRequestParameter? { get }
+    var encoding: ParameterEncoding { get }
+    
+}
